fd8abfe53c877deee0e5b17bde31670e87d35c9a
HARRISAMIN432 <144408408+HARRISAMIN432@users.noreply.github.com>
2024-02-01 23:57:12 +0500

    commit

---
+++ Week2/Game2D/Game2D/enemy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2D
+{
+	internal class enemy
+	{
+		public enemy(int eX, int eY, char[,] enemy2D)
+		{
+			X = eX;
+			Y = eY;
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 2; j++)
+				{
+					Console.SetCursorPosition(eY + j, eX + i);
+					Console.Write(enemy2D[i, j]);
+				}
+			}
+		}
+		public int X;
+		public int Y;
+	}
+}
---
+++ Week2/Game2D/Game2D/player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game2D
+{
+	internal class player
+	{
+		public player(int pX, int pY, char[,] player2D)
+		{
+			X = pX;
+			Y = pY;
+			for (int i = 0; i < 3; i++)
+			{
+				for (int j = 0; j < 7; j++)
+				{
+					Console.SetCursorPosition(Y + j, X + i);
+					Console.Write(player2D[i, j]);
+				}
+			}
+		}
+		public int X;
+		public int Y;
+	}
+}
